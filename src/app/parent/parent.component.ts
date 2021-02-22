c2709147666093da6c692e444058beee7e1d52a4
swapna <swapna.h.vishwanath@gmail.com>
2021-02-22 16:36:56 +0530

    diaplaying forms dynamically, validate and submit

--- src/app/parent/parent.component.ts
+++ src/app/parent/parent.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl } from '@angular/forms';
+import { FormControl, FormBuilder, Validators, FormGroup } from '@angular/forms';
 import { DetailsService } from '../details.service';
 
 @Component({
@@ -12,6 +12,9 @@ export class ParentComponent implements OnInit {
   EmployeeName = new FormControl('');
   formDetails;
   submissionResult;
+
+  formGroup: FormGroup;
+
   constructor(private DetailsService:DetailsService) { }
 
   ngOnInit(): void {
@@ -20,15 +23,40 @@ export class ParentComponent implements OnInit {
   getFormFields(){
   	this.DetailsService.getForm().subscribe(response=>{
       this.formDetails = response;
+      this.setupForms();
     });
   }
 
-  submitForm() {
-    const formDetails = {
-      "name": "xyz",
-      "address": "123"
-    };
-    this.DetailsService.postForm(formDetails).subscribe(response => {
+  setupForms(formDetails?: any) {
+    formDetails = formDetails || this.formDetails;
+    let group = {};
+
+    formDetails.data.form_fields.forEach(formTemplate => {
+      if(formTemplate.component === "checkbox" || formTemplate.component === "select") {
+        let boxGroup = {};
+        formTemplate.options.forEach(option => {
+          let validators = [];
+          boxGroup[option] = new FormControl((formTemplate.autoselect && formTemplate.autoselect.includes(option)) || false);
+        });
+        group[formTemplate.label] = new FormGroup(boxGroup);
+      }
+      else {
+        group[formTemplate.label] = new FormControl(formDetails.autofill || '');
+        let validators = [];
+  
+        if (formTemplate.required) validators.push(Validators.required);
+        if (formTemplate.validation) validators.push(Validators.pattern(formTemplate.validation));
+  
+        group[formTemplate.label].setValidators(validators);
+      }
+      if(!formTemplate.editable) group[formTemplate.label].disable();
+    });
+    
+    this.formGroup = new FormGroup(group);
+  }
+
+  onSubmit(){
+    this.DetailsService.postForm(this.formGroup.value).subscribe(response => {
       console.log(response);
     })
   }
